f2e1d7fcbbcefa72e1f688c6feb22413e78016e4
tihonove <tihonove@skbkontur.ru>
2017-12-30 19:04:25 +0500

    All version

--- SeleniumTesting/TestPages/versions.js
+++ SeleniumTesting/TestPages/versions.js
@@ -1,10 +1,8 @@
 var shell = require("shelljs");
 var semver = require("semver");
+
 var versionsOutput = shell.exec("npm show retail-ui versions", { silent: true });
-
 var retailUIVersions = eval(versionsOutput.stdout);
-
-// console.log(process.env);
 
 const resultVersions = {
     '16.0.0': '>=0.9.7',
@@ -13,17 +11,20 @@ const resultVersions = {
     '0.14.3': '0.6.18 || 0.8.15 || >=0.9.0',
 };
 
-const result = {};
+var result = {};
 
-var reactVersions = Object.keys(resultVersions);
-
-for(var i = 0; i < reactVersions.length; i++) {
-    var reactVersion = reactVersions[i];
-    result[reactVersion] = retailUIVersions.filter(x => semver.satisfies(x, resultVersions[reactVersion]))
+if (process.env["TEAMCITY_VERSION"]) {
+    var reactVersions = Object.keys(resultVersions);
+    for(var i = 0; i < reactVersions.length; i++) {
+        var reactVersion = reactVersions[i];
+        result[reactVersion] = retailUIVersions.filter(x => semver.satisfies(x, resultVersions[reactVersion]))
+    }
+} else {
+    result = {
+        '16.0.0': ['0.9.7'],
+        "15.4.2": ["0.9.0"],
+    };
 }
 
-// module.exports = result;
-module.exports = {
-    '16.0.0': ['0.9.7'],
-    "15.4.2": ["0.9.0"],
-};
+module.exports = result;
+
