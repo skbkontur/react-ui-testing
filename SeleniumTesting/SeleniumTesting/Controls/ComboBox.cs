31374b3459619b797784bd78e2dac484331bb5af
Eugene Molokov <molokovev@gmail.com>
2018-07-30 11:27:32 +0500

    Add IsDisabled to ComboBox

--- SeleniumTesting/SeleniumTesting/Controls/ComboBox.cs
+++ SeleniumTesting/SeleniumTesting/Controls/ComboBox.cs
@@ -19,6 +19,8 @@ namespace SKBKontur.SeleniumTesting.Controls
             this.portal = this.Find<Portal>().By("noscript");
         }
 
+        public IControlProperty<bool> IsDisabled => ReactProperty<bool>("disabled");
+
         [Obsolete]
         public string Text { get { return GetValueFromElement(x => x.Text); } }
 
